b2742ac0beff9ece5390218df0d154fc6efe8fd8
kuroi-ken <faznurramadhan2004@gmail.com>
2025-11-21 08:42:16 +0700

    Eloquent Relationship Update

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
-Route::get('/posts', function (Post $post) {
+Route::get('/posts/{post}', function (Post $post) {
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
